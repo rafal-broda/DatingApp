2fa9c2f41956b3b5e6a26cacee0fb913a5fb379e
Raf1993 <rbroda93@interia.pl>
2020-01-26 14:33:56 +0100

    End of section 5

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -17,7 +17,7 @@ export class RegisterComponent implements OnInit {
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('registration succesful')
+      console.log('registration succesful');
     }, error => {
       console.log(error);
     });
